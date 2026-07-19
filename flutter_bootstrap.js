6ab00f58950a1b5e814c78b14bd99a0d0cbe4dea
Kshitij Sharma <kshitij466e@gmail.com>
2026-07-19 23:50:24 +0530

    Deploy PortfolioV1

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"42d3d75a56efe1a2e9902f52dc8006099c45d9
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1777333062" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "3201569383" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
